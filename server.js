7d5aa48c9f0b6b875c8ee66e01a4630fe4fa4c1a
Ishu :) <104856493+Harsh-bash@users.noreply.github.com>
2023-04-27 11:25:13 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,29 @@
-import express, { json } from 'express';
-import { readFileSync } from 'fs';
-import { writeFileSync } from 'fs';
+// import express, { json } from 'express';
+// import { readFileSync } from 'fs';
+// import { writeFileSync } from 'fs';
 
+
+// const app = express();
+// const port = 4000;
+
+// app.use(json());
+
+// app.post('/api/data', (req, res) => {
+//   const data = req.body;
+//   writeFileSync('data.json', JSON.stringify(data));
+//   res.send('Data saved successfully!');
+// });
+
+// app.get('/api/data', (req, res) => {
+//   const data = readFileSync('data.json');
+//   res.send(data.toString());
+// });
+
+// app.listen(port, () => {
+//   console.log(`Server running at: http://localhost:${port}`);
+// });
+import express, { json } from 'express';
+import { readFileSync, appendFileSync } from 'fs';
 
 const app = express();
 const port = 4000;
@@ -10,7 +32,7 @@ app.use(json());
 
 app.post('/api/data', (req, res) => {
   const data = req.body;
-  writeFileSync('data.json', JSON.stringify(data));
+  appendFileSync('data.json', JSON.stringify(data));
   res.send('Data saved successfully!');
 });
 
