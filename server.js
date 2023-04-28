eaa055346a82e18f9f2b7c803cc51bab4021d8ba
Ishu :) <104856493+Harsh-bash@users.noreply.github.com>
2023-04-27 18:42:38 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,40 @@
+// import express, { json } from 'express';
+// import { readFileSync, appendFileSync } from 'fs';
+
+// const app = express();
+// const port = 4000;
+
+// app.use(json());
+
+// app.post('/api/data', (req, res) => {
+//   const data = req.body;
+//   appendFileSync('data.json', JSON.stringify(data));
+// //   res.send('Data saved successfully!');
+// });
+
+// app.get('/api/data', (req, res) => {
+//   const data = readFileSync('data.json');
+//   res.send(data.toString());
+// });
+
+// app.listen(port, () => {
+//   console.log(`Server running at: http://localhost:${port}`);
+// });
+
 import express, { json } from 'express';
 import { readFileSync, appendFileSync } from 'fs';
+import cors from 'cors';
 
 const app = express();
 const port = 4000;
 
 app.use(json());
+app.use(cors()); // Enable CORS
 
 app.post('/api/data', (req, res) => {
   const data = req.body;
   appendFileSync('data.json', JSON.stringify(data));
-//   res.send('Data saved successfully!');
+  res.send('Data saved successfully!');
 });
 
 app.get('/api/data', (req, res) => {
@@ -20,3 +45,4 @@ app.get('/api/data', (req, res) => {
 app.listen(port, () => {
   console.log(`Server running at: http://localhost:${port}`);
 });
+
